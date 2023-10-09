46c7bc84eac5ad12936a831ff3951533e9436d23
ZPitta <pittadeoliveiraj@gmail.com>
2023-10-09 13:53:38 -0300

    TryExcept implemented

--- ex049.py
+++ ex049.py
@@ -1,8 +1,13 @@
 # Desafio 49 — Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, entretanto, agora
 # um laço for.
-num = int(input("Informe um número: "))
+try:
+    num = int(input("Informe um número: "))
 
-print("*-" * 10)
-for i in range(0,11):
-    print("{0} * {1} = {2}".format(i, num, i * num))
-print("*-" * 10)
+    print("*-" * 10)
+    for i in range(0,11):
+        print("{0} * {1} = {2}".format(i, num, i * num))
+    print("*-" * 10)
+except Exception as e:
+    print(f'Não foi possível executar o programa pelo seguinte erro: {e}')
+finally:
+    print("Hello World!")
